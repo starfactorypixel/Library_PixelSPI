9bfa42b909740c01d7626b4ee680a9f3f6673466
Dragon-Knight <dubki4132@mail.ru>
2026-03-10 01:28:14 +0300

    add new driver for `W25Q128JV`

---
+++ src/drivers/SPI_W25Q128JV.h
@@ -0,0 +1,276 @@
+#pragma once
+#include <inttypes.h>
+#include <SPIManager.h>
+#include <DrakePinD.hpp>
+
+/*
+	Класс работы с SPI NOR памятью.
+	Чип: W25Q128JV https://jlcpcb.com/api/file/downloadByFileSystemAccessId/8560111703904829441
+*/
+
+class SPI_W25Q128JV : public SPIDeviceInterface
+{
+	static constexpr uint8_t CMD_WRITE_ENABLE =				0x06;
+	static constexpr uint8_t CMD_VOLATILE_SR_WRITE_ENABLE =	0x50;
+	static constexpr uint8_t CMD_WRITE_DISABLE =			0x04;
+	static constexpr uint8_t CMD_RELEASE_POWER_DOWN_ID =	0xAB;
+	static constexpr uint8_t CMD_MANUFACTURER_DEVICE_ID =	0x90;
+	static constexpr uint8_t CMD_JEDEC_ID =					0x9F;
+	static constexpr uint8_t CMD_READ_UNIQUE_ID =			0x4B;
+	static constexpr uint8_t CMD_READ_DATA =				0x03;
+	static constexpr uint8_t CMD_FAST_READ =				0x0B;
+	static constexpr uint8_t CMD_PAGE_PROGRAM =				0x02;
+	static constexpr uint8_t CMD_SECTOR_ERASE_4KB =			0x20;
+	static constexpr uint8_t CMD_BLOCK_ERASE_32KB =			0x52;
+	static constexpr uint8_t CMD_BLOCK_ERASE_64KB =			0xD8;
+	static constexpr uint8_t CMD_CHIP_ERASE_C7 =			0xC7;
+	static constexpr uint8_t CMD_CHIP_ERASE_60 =			0x60;
+	static constexpr uint8_t CMD_READ_STATUS_REGISTER_1 =	0x05;
+	static constexpr uint8_t CMD_WRITE_STATUS_REGISTER_1 =	0x01;
+	static constexpr uint8_t CMD_READ_STATUS_REGISTER_2 =	0x35;
+	static constexpr uint8_t CMD_WRITE_STATUS_REGISTER_2 =	0x31;
+	static constexpr uint8_t CMD_READ_STATUS_REGISTER_3 =	0x15;
+	static constexpr uint8_t CMD_WRITE_STATUS_REGISTER_3 =	0x11;
+	static constexpr uint8_t CMD_READ_SFDP_REGISTER =		0x5A;
+	static constexpr uint8_t CMD_ERASE_SECURITY_REGISTER =	0x44;
+	static constexpr uint8_t CMD_PROGRAM_SECURITY_REGISTER =0x42;
+	static constexpr uint8_t CMD_READ_SECURITY_REGISTER =	0x48;
+	static constexpr uint8_t CMD_GLOBAL_BLOCK_LOCK =		0x7E;
+	static constexpr uint8_t CMD_GLOBAL_BLOCK_UNLOCK =		0x98;
+	static constexpr uint8_t CMD_READ_BLOCK_LOCK =			0x3D;
+	static constexpr uint8_t CMD_INDIVIDUAL_BLOCK_LOCK =	0x36;
+	static constexpr uint8_t CMD_INDIVIDUAL_BLOCK_UNLOCK =	0x39;
+	static constexpr uint8_t CMD_ERASE_PROGRAM_SUSPEND =	0x75;
+	static constexpr uint8_t CMD_ERASE_PROGRAM_RESUME =		0x7A;
+	static constexpr uint8_t CMD_POWER_DOWN =				0xB9;
+	static constexpr uint8_t CMD_ENABLE_RESET =				0x66;
+	static constexpr uint8_t CMD_RESET_DEVICE =				0x99;
+	
+	public:
+
+		static constexpr uint32_t NOR_MAX_ADDRESS = 16777215;
+		static constexpr uint32_t NOR_MAX_PAGES = 65535;
+		static constexpr uint32_t NOR_MAX_SECTOR = 4095;
+		static constexpr uint32_t NOR_MAX_BLOCK32 = 511;
+		static constexpr uint32_t NOR_MAX_BLOCK64 = 255;
+
+		static constexpr uint32_t NOR_PAGE_SIZE = 256;
+		static constexpr uint32_t NOR_SECTOR_SIZE = 4096;
+		static constexpr uint32_t NOR_BLOCK32_SIZE = 32768;
+		static constexpr uint32_t NOR_BLOCK64_SIZE = 65536;
+		static constexpr uint32_t NOR_MEM_SIZE = 16777216;
+		
+		SPI_W25Q128JV(const DrakePin::PinD_t &cs_pin, uint32_t spi_prescaler) : SPIDeviceInterface(cs_pin, spi_prescaler)
+		{
+
+		}
+		
+		virtual void Init() override
+		{
+			DeviceActivate();
+			SendCmd1(CMD_ENABLE_RESET);
+			DeviceDeactivate();
+			
+			DeviceActivate();
+			SendCmd1(CMD_RESET_DEVICE);
+			DeviceDeactivate();
+			
+			WaitReady();
+			
+			return;
+		}
+		
+		virtual void Tick(uint32_t &time) override
+		{
+			return;
+		}
+		
+		
+		/// @brief Прочитать байты
+		/// @param address Адррес первого байта
+		/// @param length Кол-во читаемых байт
+		/// @param data Массив куда положить прочитанные данные
+		void ReadBytes(uint32_t address, uint32_t length, uint8_t *data)
+		{
+			if(address > NOR_MAX_ADDRESS) return;
+			if(WaitReady() == false) return;
+
+			DeviceActivate();
+			SendCmd4(CMD_READ_DATA, address);
+			_spi_interface->ReceiveData(data, length);
+			DeviceDeactivate();
+			
+			return;
+		}
+	
+		/// @brief Прочитать страницу, 256 байт
+		/// @warning Т.е. флешка не имеет страниц, её чтение симулируется
+		/// @param page Адрес страницы
+		/// @param data Массив куда положить прочитанные данные
+		void ReadPage(uint32_t page, uint8_t *data)
+		{
+			if(page > NOR_MAX_PAGES) return;
+
+			ReadBytes((page * NOR_PAGE_SIZE), NOR_PAGE_SIZE, data);
+			
+			return;
+		}
+
+		
+		/// @brief Записать страницу, 256 байт
+		/// @warning Т.е. флешка не имеет страниц, её запись симулируется
+		/// @param page Адрес страницы
+		/// @param data Массив откуда взять записываемые данные
+		void WritePage(uint32_t page, uint8_t *data)
+		{
+			if(page > NOR_MAX_PAGES) return;
+			if(WaitReady() == false) return;
+
+			WriteEnable();
+			DeviceActivate();
+			SendCmd4(CMD_PAGE_PROGRAM, (page * NOR_PAGE_SIZE));
+			_spi_interface->TransmitData(data, NOR_PAGE_SIZE);
+			DeviceDeactivate();
+			
+			return;
+		}
+
+		/// @brief Очистить сектор 4K
+		/// @param sector Адрес сектора
+		void EraseSector(uint32_t sector)
+		{
+			if(sector > NOR_MAX_SECTOR) return;
+			if(WaitReady() == false) return;
+
+			WriteEnable();
+			DeviceActivate();
+			SendCmd4(CMD_SECTOR_ERASE_4KB, sector);
+			DeviceDeactivate();
+			
+			return;
+		}
+
+		/// @brief Очистить блок 32К
+		/// @param block Адрес блока
+		void EraseBlock32(uint32_t block)
+		{
+			if(block > NOR_MAX_BLOCK32) return;
+			if(WaitReady() == false) return;
+
+			WriteEnable();
+			DeviceActivate();
+			SendCmd4(CMD_BLOCK_ERASE_32KB, block);
+			DeviceDeactivate();
+			
+			return;
+		}
+
+		/// @brief Очистить блок 64К
+		/// @param block Адрес блока
+		void EraseBlock64(uint32_t block)
+		{
+			if(block > NOR_MAX_BLOCK64) return;
+			if(WaitReady() == false) return;
+
+			WriteEnable();
+			DeviceActivate();
+			SendCmd4(CMD_BLOCK_ERASE_64KB, block);
+			DeviceDeactivate();
+			
+			return;
+		}
+
+		/// @brief Очистить всю память чипа
+		void EraseChip()
+		{
+			if(WaitReady() == false) return;
+
+			WriteEnable();
+			DeviceActivate();
+			SendCmd1(CMD_CHIP_ERASE_60);
+			DeviceDeactivate();
+			
+			return;
+		}
+		
+		void WriteEnable()
+		{
+			DeviceActivate();
+			SendCmd1(CMD_WRITE_ENABLE);
+			DeviceDeactivate();
+
+			return;
+		}
+		
+		bool WaitReady(uint32_t delay = 100000)
+		{
+			while(delay--)
+			{
+				if((ReadStatus1() & 0x01) == 0)
+					return true;
+			}
+			
+			return false;
+		}
+		
+		uint8_t ReadStatus1()
+		{
+			uint8_t status;
+			
+			DeviceActivate();
+			SendCmd1(CMD_READ_STATUS_REGISTER_1);
+			_spi_interface->ReceiveData(&status, 1);
+			DeviceDeactivate();
+			
+			return status;
+		}
+		
+		template<uint8_t N> 
+		void ReadDevID(uint8_t (&data)[N])
+		{
+			static_assert(N >= 3, "Buffer too small for DevID");
+			
+			DeviceActivate();
+			SendCmd1(CMD_JEDEC_ID);
+			_spi_interface->ReceiveData(data, 3);
+			DeviceDeactivate();
+			
+			return;
+		}
+		
+		template<uint8_t N> 
+		void ReadUniqueID(uint8_t (&data)[N])
+		{
+			static_assert(N >= 8, "Buffer too small for UniqueID");
+			
+			DeviceActivate();
+			uint8_t tx[5] = {CMD_READ_UNIQUE_ID, 0x00, 0x00, 0x00, 0x00};
+			_spi_interface->TransmitData(tx, sizeof(tx));
+			_spi_interface->ReceiveData(data, 8);
+			DeviceDeactivate();
+			
+			return;
+		}
+		
+		void SendCmd1(uint8_t cmd)
+		{
+			uint8_t data[1] = {0x00};
+			data[0] = cmd;
+			
+			_spi_interface->TransmitData(data, sizeof(data));
+		}
+		
+		void SendCmd4(uint8_t cmd, uint32_t address)
+		{
+			uint8_t data[4] = {0x00};
+			data[0] = cmd;
+			data[1] = (address >> 16) & 0xFF;
+			data[2] = (address >> 8) & 0xFF;
+			data[3] = address & 0xFF;
+			
+			_spi_interface->TransmitData(data, sizeof(data));
+		}
+
+	private:
+
+};
